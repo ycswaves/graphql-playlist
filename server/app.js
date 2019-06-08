1bd8373b49f4582fd44a15dd00236d82a4db8d20
chenshu <ycswaves@gmail.com>
2019-06-08 09:14:03 +0800

    1st commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema
+  })
+);
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`now listening for requests on port ${PORT}`);
+});
