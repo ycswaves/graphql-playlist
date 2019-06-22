bc88c2f2a9135d6874ac3b78eb8dd50f85836669
chenshu <ycswaves@gmail.com>
2019-06-22 15:41:43 +0800

    complete server side and start front end using CRA

--- server/app.js
+++ server/app.js
@@ -1,13 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
+const { DB_HOST, DB_USER, DB_PASS } = process.env;
 const app = express();
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}`);
+mongoose.connection.once('open', () => {
+  console.log('connected to db');
+});
 
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema
+    schema,
+    graphiql: true
   })
 );
 
